9309304416121229c32c2cb27f43a86ea1a49c47
LuigiRac <lui.rac06@gmail.com>
2025-05-08 14:11:14 +0200

    Moved ProjectController to Admin; Added Authentication

--- laravel-portfolio/routes/web.php
+++ laravel-portfolio/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,6 +28,6 @@ Route::middleware(['auth', 'verified'])
 
 });
 
-Route::resource('project', ProjectController::class);
+Route::resource('project', ProjectController::class) ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
