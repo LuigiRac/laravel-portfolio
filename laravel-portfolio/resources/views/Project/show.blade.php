698e3379afd35a6dda4fc90827b9257d957cf27c
LuigiRac <lui.rac06@gmail.com>
2025-05-12 19:39:16 +0200

    Added modal in delete

--- laravel-portfolio/resources/views/Project/show.blade.php
+++ laravel-portfolio/resources/views/Project/show.blade.php
@@ -15,14 +15,30 @@
   <div class="container text-center d-flex py-4 gap-3">
     <a class="btn btn-warning" href="{{route('project.edit', $project)}}">Modifica</a>
 
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Elimina
+  </button>
 
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina il progetto</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Sei sicuro di voler eliminare il progetto?
+        </div>
+      <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+          
     <form action="{{route('project.destroy', $project)}}" method="POST">
       @csrf
       @method('DELETE')
      <input type="submit" class="btn btn-danger" value="Elimina">
- </form>
-
+    </form>
   </div>
+</div>
 
   
 @endsection
